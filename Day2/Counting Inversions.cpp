abe4f9a3d02fa7cf85563fe89566a0b29380dd2f
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-27 21:14:33 +0530

    Update Counting Inversions.cpp

--- Day2/Counting Inversions.cpp
+++ Day2/Counting Inversions.cpp
@@ -3,6 +3,16 @@
 
 // Approach : Time Complexity: O(N*log(N)), Space Complexity: O(N)
 
+// Algorithm: 
+// The idea is similar to merge sort, divide the array into two equal or almost equal halves in each step until the base case is reached.
+// Create a function merge that counts the number of inversions when two halves of the array are merged, create two indices l1 and r1,
+// l1 is the index for the first half, and l2 is an index of the second half. if a[l1] > a[l2], then there are (mid – l1) inversions. 
+// because left and right subarrays are sorted, so all the remaining elements in left-subarray (a[l1+1], a[l2+2] … a[r1]) will be greater than a[l2].
+// Create a recursive function to divide the array into halves and find the answer by summing the number of inversions is the first half, 
+// the number of inversion in the second half and the number of inversions by merging the two.
+// The base case of recursion is when there is only one element in the given half.
+// Print the answer
+
 long long merge(long long arr[],long long temp[],long long l,long long mid,long long r){
         long long int l1=l,r1=mid-1,l2=mid,r2=r;
         long long cnt=0;
