318b6b396eef4ffe8738a7db00fd7fe4c7925612
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-08-28 12:18:48 +0530

    Update Pow(x, n).cpp

--- Day 3/Pow(x, n).cpp
+++ Day 3/Pow(x, n).cpp
@@ -50,3 +50,21 @@ double myPow(double x, int n) {
             }
             return ans;
     }
+// 2nd Implementation
+
+double myPow(double x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+         double p=myPow(x,n/2); 
+        if(n%2){
+            if(n<0){
+                return (1/x)*p*p;
+            }
+            else
+            return x*p*p;
+        }
+            return p*p;
+        
+    }
