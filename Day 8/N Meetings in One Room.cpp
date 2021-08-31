547cea12b629aa5f300c3c8e5494e8cea0c0e967
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-01 02:35:50 +0530

    Update N Meetings in One Room.cpp

--- Day 8/N Meetings in One Room.cpp
+++ Day 8/N Meetings in One Room.cpp
@@ -26,3 +26,32 @@ static bool comp(pair<int,int> &p1,pair<int,int> &p2){
         }
         return ans;
     }
+
+// Modification of above problem:
+
+// Problem Link:  https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+static bool comp(vector<int> &v1,vector<int> &v2){
+        if(v1[1]==v2[1]){
+            return v1[0]<v2[0];
+        }
+        return v1[1]<v2[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end(),comp);
+        int n=arr.size();
+        if(n==1)
+            return 1;
+        int s=arr[0][0],e=arr[0][1];
+        int ans=1;
+        int i;
+        for(i=1;i<n;i++){
+            if(arr[i][0]>e){
+                ans++;
+                s=arr[i][0];
+                e=arr[i][1];
+            }
+
+        }
+        return ans;
+    }
