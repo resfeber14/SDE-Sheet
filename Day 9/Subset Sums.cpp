a91d8abf3b9dab19f99287ec189263aa3f9b5d20
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-01 14:07:25 +0530

    Update Subset Sums.cpp

--- Day 9/Subset Sums.cpp
+++ Day 9/Subset Sums.cpp
@@ -26,20 +26,18 @@ void solve(int i,int curr,vector<int> &v,vector<int> &arr,int n){
 
 // Take a temp vector and traverse the array and push everytime and then call the recursion the pop back
 
-void solve(int i,int curr,vector<int> &v,vector<int> &arr,int n){
-        if(i==n){
-            v.push_back(curr);
-            return ;
-        }
-        solve(i+1,curr,v,arr,n);
-        solve(i+1,curr+arr[i],v,arr,n);
-        
+void solve(int i,int n,vector<int> &arr,vector<vector<int> > &v,vector<int> temp){
+        v.push_back(temp);
+       for(int j=i;j<n;j++){
+           temp.push_back(arr[j]);
+           solve(j+1,n,arr,v,temp);
+           temp.pop_back();
+       }
     }
-    
-    vector<int> subsetSums(vector<int> arr, int n){
-        vector<int> v;
-        solve(0,0,v,arr,n);
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int>> v;
+        vector<int> temp;
+        solve(0,arr.size(),arr,v,temp);
         return v;
-        
     }
 
