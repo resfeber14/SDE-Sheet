2c32cc415d4d60701e68312835864e9715098ea7
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-01 17:27:25 +0530

    Update Combination Sum II.cpp

--- Day 9/Combination Sum II.cpp
+++ Day 9/Combination Sum II.cpp
@@ -32,3 +32,33 @@ void solve(set<vector<int>> &v,int k,int i,vector<int> &temp,vector<int>& arr){
         }
         return v;
     }
+
+// Approach 2: time complexity: O(2^n * n)   Space Complexity: Unknown
+
+// Same as subsets II
+// Sort the array and run a loop to check if the current element is less than the k or not.if yes then take it
+// else don't take it
+
+
+void solve(int i,int k,vector<int> temp,vector<vector<int> > &v,vector<int> &arr){
+        if(k==0){
+                v.push_back(temp);
+                return;
+            }
+        for(int j=i;j<arr.size();j++){
+            if(i!=j && arr[j]==arr[j-1])
+                continue;
+            if(k>=arr[j]){
+                temp.push_back(arr[j]);
+                solve(j+1,k-arr[j],temp,v,arr);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        vector<vector<int> > v;
+        vector<int> temp;
+        solve(0,target,temp,v,arr);
+        return v;
+    }
