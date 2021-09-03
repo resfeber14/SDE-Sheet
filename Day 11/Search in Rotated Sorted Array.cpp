e737a8a1554097b8b02835c975685239bd3378c1
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-03 13:21:09 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ Day 11/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,37 @@
+// Problem Link:  https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+// Approach: Time complexity: O(N) Space Complexity: O(1)
+// Linearly traverse the array
+
+// Approach 2: Time complexity: O(logN) Space Complexity: O(1)
+
+// Check which part of the array is sorted whether its right half or the right half
+// If left half is sorted then check if target lies in left side or in the right side
+
+
+int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int l=0,r=arr.size()-1;
+        while(l<=r){
+            int mid=(l+r)>>1;
+            if(arr[mid]==target)
+                return mid;
+            if(arr[l]<=arr[mid]){
+                if(target>=arr[l] && target<arr[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else if(arr[mid]<=arr[r]){
+                if(target>arr[mid] && target<=arr[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
