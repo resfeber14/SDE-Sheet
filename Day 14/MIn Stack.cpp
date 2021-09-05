8648b2b3f846b31a8ebc567456777ee45d0840fa
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-05 15:50:47 +0530

    Create MIn Stack.cpp

---
+++ Day 14/MIn Stack.cpp
@@ -0,0 +1,39 @@
+// Problem Link:  https://leetcode.com/problems/min-stack/
+
+// Approach: Time complexity: O(N)  Space complexity: O(N)
+
+// Store element in a stack
+// To find minimum element in O(1) Time,use another stack in which we will push minimum element till now into that
+// When we push an element in the stack we check if in the second stack if its top is greater than the element whoch we are gonna push
+// If it greater then,we need to push the element in the both stacks
+
+
+stack<int> s,s1;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(s1.empty()){
+            s1.push(val);
+        }
+        else if(s1.top()>=val){
+            s1.push(val);
+        }
+    }
+    
+    void pop() {
+        if(s1.top()==s.top()){
+            s1.pop();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return s1.top();
+    }
