59378b12b625202c19f171f42371514e48121a8e
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-09-05 12:44:38 +0530

    Update Largest Rectangle in Histogram.cpp

--- Day 13(Stacks and Queues)/Largest Rectangle in Histogram.cpp
+++ Day 13(Stacks and Queues)/Largest Rectangle in Histogram.cpp
@@ -1,10 +1,10 @@
 // Problem Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
 
-// Approach 1: Time complexity: O(N) Space Complexity: O(4*n)
+// Approach 1: Time complexity: O(N*N) Space Complexity: O(4*n)
 // Traverse the whole array Linearly for finding next smaller element to the left and to the right of the current element and find the current maximum
 // Rectangle of the histogram and thus find the maximum one
 
-// Approach 2: Time complexity: O(n*n) Space Complexity: O(4*n)
+// Approach 2: Time complexity: O(N) Space Complexity: O(3*n)
 // We are considering the current element's height to be the maximum height and find next smaller element to its left and right
 // Use stack to find the next smaller element to the left and to the right of the current element 
 
