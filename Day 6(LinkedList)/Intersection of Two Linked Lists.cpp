2ef36965dd75899534ed64a69c679d318c124a4f
Shweta Kumari <61600237+resfeber14@users.noreply.github.com>
2021-11-09 17:54:15 +0530

    Update Intersection of Two Linked Lists.cpp

--- Day 6(LinkedList)/Intersection of Two Linked Lists.cpp
+++ Day 6(LinkedList)/Intersection of Two Linked Lists.cpp
@@ -92,7 +92,10 @@ ListNode *getIntersectionNode(ListNode *l1, ListNode *l2) {
     }
 /*
 Approach 4:   Time Complexity: O(2*M) Space Complexity: O(1)
-
+We can use two iterations to do that. In the first iteration, we will reset the pointer of one linkedlist to the head of another linkedlist after it reaches the tail node.
+In the second iteration, we will move two pointers until they points to the same node. Our operations in first iteration will help us counteract the difference. 
+So if two linkedlist intersects, the meeting point in second iteration must be the intersection point. If the two linked lists have no intersection at all, 
+then the meeting pointer in second iteration must be the tail node of both lists, which is null
 
 */
 ListNode *getIntersectionNode(ListNode *l1, ListNode *l2) {
